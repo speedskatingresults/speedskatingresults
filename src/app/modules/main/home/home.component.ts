60e92fafa0807698e32dce7cd9290ccfdc20445c
20Koen02 <mail@koen02.nl>
2021-03-22 21:44:03 +0100

    some homepage widgets

--- src/app/modules/main/home/home.component.ts
+++ src/app/modules/main/home/home.component.ts
@@ -1,18 +1,31 @@
-import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import {ChangeDetectionStrategy, Component, ViewChild, ViewEncapsulation} from '@angular/core';
+import {MatTableDataSource} from "@angular/material/table";
+import {MatSort} from "@angular/material/sort";
 
 @Component({
-    selector       : 'example',
-    templateUrl    : './home.component.html',
-    styleUrls      : ['./home.component.scss'],
-    encapsulation  : ViewEncapsulation.None,
-    changeDetection: ChangeDetectionStrategy.OnPush
+  selector: 'example',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class HomeComponent
-{
-    /**
-     * Constructor
-     */
-    constructor()
-    {
-    }
+export class HomeComponent {
+  recentCompDataSource: MatTableDataSource<any>;
+  recentCompTableColumns: string[];
+
+  @ViewChild('recentCompTable', {read: MatSort})
+  recentCompTableMatSort: MatSort;
+
+  /**
+   * Constructor
+   */
+  constructor() {
+    this.recentCompDataSource = new MatTableDataSource();
+    this.recentCompTableColumns = ['transactionId', 'date', 'name', 'amount', 'status'];
+        
+  }
+
+  trackByFn(index: number, item: any): any {
+    return item.id || index;
+  }
 }
