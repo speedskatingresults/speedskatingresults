128850ad69255e49a65670917911be0ae92715c1
MeesEgberts <mees.egberts2001@gmail.com>
2021-03-25 22:21:47 +0100

    added skater search

--- src/app/modules/main/skaters/skaters.module.ts
+++ src/app/modules/main/skaters/skaters.module.ts
@@ -11,8 +11,6 @@ import {MatProgressBarModule} from '@angular/material/progress-bar';
 import {SkatersComponent} from './skaters.component';
 import {SkatersRoutes} from './skaters.routing';
 import { ListComponent } from './list/list.component';
-import { SearchComponent } from './search/search.component';
-import { ItemComponent } from './list/item/item.component';
 import {SearchModule} from '../../../layout/common/search/search.module';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
