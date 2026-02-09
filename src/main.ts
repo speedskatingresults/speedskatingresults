88c9cdb8e12b0cd106d10ecc56406ff2d532fbfb
NielsPrins <NielsPrins@users.noreply.github.com>
2026-02-09 20:48:27 +0100

    Upgrade to angular 12

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import {enableProdMode} from '@angular/core';
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 import {AppModule} from './app/app.module';
 import {environment} from './environments/environment';
-import 'zone.js/dist/zone';
+import 'zone.js';
 
 if (environment.production) {
   enableProdMode();
